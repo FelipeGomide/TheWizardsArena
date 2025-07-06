c96990379c0e70f8f6da652fdf65d0283a8dcdc4
Felipe Gomide <flgomide@gmail.com>
2025-07-06 16:24:10 -0300

    Padronizados sprites do mapa, inimigos e personagens no Tiled

---
+++ Assets/Levels/Bricks.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Level Objects" tilewidth="48" tileheight="50" tilecount="8" columns="0" tilerendersize="grid">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../Sprites/Blocks/CenterBrick.png" width="18" height="18"/>
+ </tile>
+ <tile id="1">
+  <image source="../Sprites/Blocks/LeftBrick.png" width="18" height="18"/>
+ </tile>
+ <tile id="2">
+  <image source="../Sprites/Blocks/RightBrick.png" width="18" height="18"/>
+ </tile>
+ <tile id="3">
+  <image source="../Sprites/Blocks/Platform.png" width="18" height="18"/>
+ </tile>
+ <tile id="4">
+  <image source="../Sprites/Blocks/Spikes.png" width="18" height="18"/>
+ </tile>
+ <tile id="5" x="0" y="0" width="32" height="40">
+  <image source="../Sprites/Ghost/Ghost_normal_angry.png" width="256" height="40"/>
+ </tile>
+ <tile id="6" x="0" y="0" width="48" height="48">
+  <image source="../Sprites/Goblin/GoblinSpriteSheet.png" width="192" height="48"/>
+ </tile>
+ <tile id="7" x="30" y="0" width="40" height="50">
+  <image source="../Sprites/Warrior/Warrior.png" width="288" height="144"/>
+ </tile>
+</tileset>
